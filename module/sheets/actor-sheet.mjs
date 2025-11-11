982c764c03370c219fd0e4f260028c37edb831e2
Mpocheron <mateo.pocheron@gmail.com>
2025-11-11 21:00:02 +0100

    Actor sheet and basic character sheet visuals

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -1,3 +1,18 @@
-export class ICONActorSheet extends ActorSheet {
+export class IconActorSheet extends ActorSheet {
     
+    /** @Override */
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ['icon', 'sheet', 'acctor'],
+            template: 'systems/ICON/templates/actor/actor-sheet.html',
+            width: 600,
+            height: 600,
+            tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'info' }]
+        });
+    }
+
+    /** @Override */
+    get template() {
+        return 'systems/ICON/templates/actor/actor-${this.actor.type}-sheet.html';
+    }
 }
